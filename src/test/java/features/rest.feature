e48f1685c147a39a970e6ba2b651d3ea734ac053
Sumit Ghosh <ghosh.sumit1@gmail.com>
2022-05-08 20:55:41 +0530

    update

---
+++ src/test/java/features/rest.feature
@@ -0,0 +1,12 @@
+@Rest
+Feature: Record a new trade
+
+  @RestDemo
+  Scenario: Validate the post request
+    Given the following trade:
+      | security | buySell | quantity | priceInCents |
+      | APPL     | BUY     |      200 |        10000 |
+    When I record the trade
+    Then the recorded trade should include the following details:
+      | security | buySell | quantity | priceInCents |
+      | APPL     | BUY     |      200 |        10000 |
---
+++ src/test/java/runner/TestRunner1.java
@@ -0,0 +1,23 @@
+package runner;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefination", plugin = { "pretty",
+		"html:target/cucumber-reports.html", "json:target/cucumber.json",
+		}, tags = "@RestDemo")
+
+public class TestRunner1 {
+	@AfterClass
+	public static void sendEmail() {
+
+		System.out.println("Send Email of execution");
+		
+	}
+}
